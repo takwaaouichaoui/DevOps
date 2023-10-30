9fd9b05bff8034591464cfae5be6087d04d4023c
Yasminekaraoui <yasmine.karaoui@esprit.tn>
2023-10-30 21:16:40 +0100

    first

--- src/test/java/tn/esprit/rh/achat/MockitoTest.java
+++ src/test/java/tn/esprit/rh/achat/MockitoTest.java
@@ -42,38 +42,7 @@ public class MockitoTest {
 
     private StockServiceImpl service;
     private StockRepository repository;
-//    private MockMvc mockMvc;
-//    ObjectMapper objectMapper = new ObjectMapper();
-//    ObjectWriter objectWriter = objectMapper.writer();
-//@Mock
-// private StockRepository stockRepository;
-//@InjectMocks
-//    private StockRestController stockRestController;
-//
-//    long id1 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    long id3 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
-//    Stock RECORD_1 = new Stock(id1,"alimentaire",50,5);
-//    Stock RECORD_2 = new Stock(id2,"électro",100,20);
-//    Stock RECORD_3 = new Stock(id3,"vestimentaire",150,10);
-//@Before
-//    public void setUp(){
-//    MockitoAnnotations.initMocks(this);
-//    this.mockMvc = MockMvcBuilders.standaloneSetup(stockRestController).build();
-//
-//   }
-//@Test
-////    public void getAllRecords_success()throws Exception{
-//
-//    List<Stock> records = new ArrayList<>(Arrays.asList(RECORD_1,RECORD_2,RECORD_3));
-//    Mockito.when(stockRepository.findAll()).thenReturn(records);
-//    mockMvc.perform(MockMvcRequestBuilders
-//            .get("/retrieve-all-stocks")
-//            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk())
-//            .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(3)))
-//            .andExpect((ResultMatcher) jsonPath("$[2].libelleStock", is("alimentaire")));
-//}
+
 
     @Test
     public void getStockTest(){
@@ -85,7 +54,7 @@ public class MockitoTest {
         service = new StockServiceImpl(repository);
 
         List<Stock> stockList = new ArrayList<>();
-        stockList.add(new Stock(id,"alimentaire",100,5));
+        stockList.add(new Stock(id,"alimsentaire",100,5));
         stockList.add(new Stock(id2,"vest",50,10));
         when(repository.findAll()).thenReturn(stockList);
 
